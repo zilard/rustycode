93863844f5aeb4bbf642c081bca910890543ce38
zilard <zilard.cherry@gmail.com>
2023-05-20 22:48:19 +0200

    use generic lifetime annotations

--- lifetime2/src/main.rs
+++ lifetime2/src/main.rs
@@ -2,11 +2,22 @@ fn main() {
     let string1 = String::from("abcd");
     let string2 = String::from("xyz");
 
+// how does the borrowchecker know that result is not a dangling reference
+// we just told the borrowchecker that
+// whatever gets returned from 'longest' will have a lifetime equal to the smallest lifetime being
+// passed in
+
     let result = longest(string1.as_str(), string2.as_str());
     println!("The longest string is {}", result);
 }
 
-fn longest(x: &str, y: &str) -> &str {
+// generic lifetime annotations dont actually change the lifetim 
+// they just create a relationship between lifetimes of multiple references
+
+// the lifetime of the returned reference will be the same as the smallest lifetime of the
+// arguments
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
