f9a78d0d83b9c87b6d76327c632dd226c06ed0d3
zilard <zilard.cherry@gmail.com>
2023-05-20 22:38:29 +0200

    expected named lifetime parameter

---
+++ lifetime2/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = String::from("xyz");
+
+    let result = longest(string1.as_str(), string2.as_str());
+    println!("The longest string is {}", result);
+}
+
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
